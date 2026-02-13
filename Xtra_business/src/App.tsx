1689b1a413a932dd6bb60db0c29725de61db270b
Tech_Bishop <bentaller10@gmail.com>
2026-02-13 16:22:24 +0200

    Update App.tsx
    deleted unused components

--- Xtra_business/src/App.tsx
+++ Xtra_business/src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header";
 import Great from "./components/Great";
 import About from "./components/ThankYou";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
